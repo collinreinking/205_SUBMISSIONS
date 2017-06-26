cff002895fda3599c261d2ef8fc98d84209f0165
collinreinking <collin.reinking@berkeley.edu>
2017-06-26 13:48:56 -0700

    first try at have_base_ddl

---
+++ exercise_1/loading_and_modelling/hive_base_ddl.sql
@@ -0,0 +1,51 @@
+DROPTABLE hospitals;
+CREATE EXTERNAL TABLE hospitals (
+Provider_ID string,
+Hospital_Name string,
+Address string,
+City string,
+State string,
+ZIP_Code string,
+County_Name string,
+Phone_Number string,
+Hospital_Type string,
+Hospital_Ownership string,
+Emergency_Services string,
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+  "separatorChar" = ",",
+  "quoteChar" = '"',
+  "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION ‘hospital_compare/hositals’;
+
+
+DROPTABLE effective_care;
+CREATE EXTERNAL TABLE effective_care (
+Provider_ID String,
+Hospital_Name String,
+Address String,
+City String,
+State String,
+ZIP_Code String,
+County_Name String,
+Phone_Number String,
+Condition String,
+Measure_ID String,
+Measure_Name String,
+Score String,
+Sample String,
+Footnote String,
+Measure_Start_Date String,
+Measure_End_Date String
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+  "separatorChar" = ",",
+  "quoteChar" = '"',
+  "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION ‘hospital_compare/effective_care’;
