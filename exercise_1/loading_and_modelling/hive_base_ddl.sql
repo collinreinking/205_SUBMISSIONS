59ba747a12fd2a6325460a73a05fec02fe16df0d
collinreinking <collin.reinking@berkeley.edu>
2017-06-27 02:27:50 +0000

    sending back to local

--- exercise_1/loading_and_modelling/hive_base_ddl.sql
+++ exercise_1/loading_and_modelling/hive_base_ddl.sql
@@ -19,7 +19,7 @@ WITH SERDEPROPERTIES (
   "escapeChar" = '\\'
 )
 STORED AS TEXTFILE
-LOCATION '/user/w205/hospital_compare/hositals';
+LOCATION '/user/w205/hospital_compare/hospitals/';
 
 
 DROP TABLE effective_care;
@@ -49,3 +49,97 @@ WITH SERDEPROPERTIES (
 )
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/effective_care';
+
+
+DROP TABLE readmissions;
+CREATE EXTERNAL TABLE readmissions (
+Provider_ID String,
+Hospital_Name String,
+Address String,
+City String,
+State String,
+ZIP_Code String,
+County_Name String,
+Phone_Number String,
+Measure_Name String,
+Measure_ID String,
+Compared_to_National String,
+Denominator String,
+Score String,
+Lower_Estimate String,
+Higher_Estimate String,
+Footnote String,
+Measure_Start_Date String,
+Measure_End_Date String
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+  "separatorChar" = ",",
+  "quoteChar" = '"',
+  "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/readmissions';
+
+DROP TABLE measures;
+CREATE EXTERNAL TABLE measures (
+Measure_Name String,
+Measure_ID String,
+Measure_Start_Quarter String,
+Measure_Start_Date String,
+Measure_End_Quarter String,
+Measure_End_Date
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+  "separatorChar" = ",",
+  "quoteChar" = '"',
+  "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/measures';
+
+DROP TABLE surveys_responses;
+CREATE EXTERNAL TABLE surveys_responses (
+Provider_Number String,
+Hospital_Name  String,
+Address String,
+City String,
+State String,
+ZIP_Code String,
+County_Name String,
+Communication_with_Nurses_Achievement_Points String,
+Communication_with_Nurses_Improvement_Points String,
+Communication_with_Nurses_Dimension_Score String,
+Communication_with_Doctors_Achievement_Points String,
+Communication_with_Doctors_Improvement_Points String,
+Communication_with_Doctors_Dimension_Score String,
+Responsiveness_of_Hospital_Staff_Achievement_Points String,
+Responsiveness_of_Hospital_Staff_Improvement_Points String,
+Responsiveness_of_Hospital_Staff_Dimension_Score String,
+Pain_Management_Achievement_Points String,
+Pain_Management_Improvement_Points String,
+Pain_Management_Dimension_Score String,
+Communication_about_Medicines_Achievement_Points String,
+Communication_about_Medicines_Improvement_Points String,
+Communication_about_Medicines_Dimension_Score String,
+Cleanliness_and_Quietness_of_Hospital_Environment_Achievement_Points String,
+Cleanliness_and_Quietness_of_Hospital_Environment_Improvement_Points String,
+Cleanliness_and_Quietness_of_Hospital_Environment_Dimension_Score String,
+Discharge_Information_Achievement_Points String,
+Discharge_Information_Improvement_Points String,
+Discharge_Information_Dimension_Score String,
+Overall_Rating_of_Hospital_Achievement_Points String,
+Overall_Rating_of_Hospital_Improvement_Points String,
+Overall_Rating_of_Hospital_Dimension_Score String,
+HCAHPS_Base_Score String,
+HCAHPS_Consistency_Score
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+  "separatorChar" = ",",
+  "quoteChar" = '"',
+  "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/surveys_responses';
--- exercise_1/loading_and_modelling/load_data_lake.sh
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -1,5 +1,5 @@
 #get data from internet, unzip into new directory, delete zip file
-wget -O Hospital_Revised_Flatfiles.zip https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
+wget -O Hospital_Revised_Flatfiles.zip http://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
 unzip Hospital_Revised_Flatfiles.zip -d ./Hospital_Revised_Flatfiles
 rm Hospital_Revised_Flatfiles.zip
 
@@ -19,9 +19,16 @@ rm -rf Hospital_Revised_Flatfiles
 #Create hdfs directory for data
 hdfs dfs -mkdir /user/w205/hospital_compare
 
+#Create hdfs directory for each data table
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir /user/w205/hospital_compare/measures
+hdfs dfs -mkdir /user/w205/hospital_compare/surveys_responses
+
 #Load data into hdfs
-hdfs dfs -put base_data/hospitals.csv hospitals
-hdfs dfs -put base_data/effective_care.csv effective_care
-hdfs dfs -put base_data/readmissions.csv readmissions
-hdfs dfs -put base_data/measures.csv measures
-hdfs dfs -put base_data/surveys_responses.csv surveys_responses
+hdfs dfs -put base_data/hospitals.csv hospital_compare/hospitals/hospitals
+hdfs dfs -put base_data/effective_care.csv hospital_compare/effective_care/effective_care
+hdfs dfs -put base_data/readmissions.csv hospital_compare/readmissions/readmissions
+hdfs dfs -put base_data/measures.csv hospital_compare/measures/measures
+hdfs dfs -put base_data/surveys_responses.csv hospital_compare/surveys_responses/surveys_responses
